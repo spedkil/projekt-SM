762239ed8284b70024a8c277add24f71ecbde3f9
junqy <popek1814@gmail.com>
2022-01-28 21:37:43 +0100

    notification

--- app/src/main/java/com/example/projekt_sm/MainActivity.java
+++ app/src/main/java/com/example/projekt_sm/MainActivity.java
@@ -1,10 +1,18 @@
 package com.example.projekt_sm;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.example.projekt_sm.helpers.ImageHelperActivity;
 import com.example.projekt_sm.image.FlowerClassificationActivity;
@@ -19,6 +27,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button notifyBtn;
+        createNotificationChannel();
+        notifyBtn = findViewById(R.id.notify_button);
+        Intent intent = new Intent(this, MainActivity.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addNextIntentWithParentStack(intent);
+        PendingIntent pendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+
+
+
+        notifyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //notification code
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this, "Notification");
+                builder.setContentTitle(getString((R.string.notify_title)));
+                builder.setContentText(getString(R.string.notify_text));
+                builder.setSmallIcon(R.drawable.ic_notify);
+                builder.setPriority(NotificationCompat.PRIORITY_HIGH);
+                builder.setContentIntent(pendingIntent);
+                builder.setAutoCancel(true);
+
+                NotificationManagerCompat managerCompat = NotificationManagerCompat.from(MainActivity.this);
+                managerCompat.notify(1, builder.build());
+            }
+        });
+
+    }
+
+    public void createNotificationChannel(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel channel = new NotificationChannel("Notification", "Notification", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
     }
     public void onGotoImageActivity(View view){
         Intent intent = new Intent(this, ImageClassificationActivity.class);
