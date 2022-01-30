2527645c10a08367d3d4e44c5eba5f02fee73040
junqy <popek1814@gmail.com>
2022-01-30 20:07:05 +0100

    wyszukiwanie kategorii

--- app/src/main/java/com/example/projekt_sm/ImageActivity.java
+++ app/src/main/java/com/example/projekt_sm/ImageActivity.java
@@ -3,12 +3,18 @@ package com.example.projekt_sm;
 import static com.example.projekt_sm.R.layout.*;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.AbsListView;
+import android.widget.EditText;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -25,6 +31,7 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 public class ImageActivity extends AppCompatActivity {
@@ -36,6 +43,7 @@ public class ImageActivity extends AppCompatActivity {
 
     Boolean isScrolling = false;
     int currentItems, totalItems, scrollOutItems;
+    String url = "https://api.pexels.com/v1/curated/?page="+page+"&per_page=80";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,7 +83,7 @@ public class ImageActivity extends AppCompatActivity {
     }
 
     public void fetchImage(){
-        StringRequest request = new StringRequest(Request.Method.GET, "https://api.pexels.com/v1/curated/?page="+page+"&per_page=80",
+        StringRequest request = new StringRequest(Request.Method.GET, url,
                 new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
@@ -117,5 +125,42 @@ public class ImageActivity extends AppCompatActivity {
         };
         RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
         requestQueue.add(request);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==R.id.search){
+            AlertDialog.Builder alert = new AlertDialog.Builder(this);
+            final EditText editText = new EditText(this);
+            editText.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+            alert.setMessage("Enter image category..");
+            alert.setTitle("Search");
+            alert.setView(editText);
+            alert.setPositiveButton("Search", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    String query = editText.getText().toString().toLowerCase();
+                    url = "https://api.pexels.com/v1/search/?page="+page+"&per_page=80&query="+query;
+                    imageModelList.clear();
+                    fetchImage();
+                }
+            });
+            alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
+                }
+            });
+
+            alert.show();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
